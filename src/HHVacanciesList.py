4bd97c41e7c8a340542b6838e3aad7f609293575
Alyona Starkova <alyona.starrkova@yandex.ru>
2024-02-27 21:39:29 +0500

    Add cls HHVacanciesList

---
+++ src/HHVacanciesList.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+import requests
+import json
+
+class AbstractApi(ABC):
+    """абстрактный класс для работы с API сервиса с вакансиями"""
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+
+
+class HHVacanciesList(AbstractApi):
+    """класс для работы с hh.ru"""
+
+    def __init__(self, vacancy):
+        """Инициализатор класса, vacancy - название вакансии для поиска"""
+        self.vacancy = vacancy
+        self.vacancies = self.get_vacancies()
+
+
+    def get_vacancies(self):
+        """Получает список вакансий"""
+        response = requests.get('https://api.hh.ru/vacancies', {'text': self.vacancy, 'per_page': 100})
+        vacancies = json.loads(response.text)['items']
+        return vacancies
+
+
+
+example = HHVacanciesList('python')
+list = example.get_vacancies()
+print(list)
